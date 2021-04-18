9d8c7ee675b0a0c126de10b673b820799edd1671
umeshthapa12 <Dell@UMESH>
2021-04-18 18:52:20 +0545

    update_recyclebin

--- src/components/other/List.vue
+++ src/components/other/List.vue
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */ /* eslint-disable prefer-const */
 <template>
   <div class="row">
     <div class="col-md-4"></div>
@@ -22,7 +23,7 @@
         </div>
       </div>
 
-      <h3 class="mt-4 pt-4">List {{ name }}</h3>
+      <h3 class="mt-4 pt-4">List</h3>
       <table class="table table-striped">
         <thead>
           <tr>
@@ -66,6 +67,47 @@
       >
         Data not found!!!
       </h4>
+
+      <hr />
+      <h3 class="mt-4 pt-4">Recycle bin</h3>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in deletedItems" :key="i.id">
+            <th><input type="checkbox" aria-label="Checkbox" /></th>
+            <td>
+              <span class="badge badge-primary badge-pill">{{ i.id }}</span>
+            </td>
+            <td>{{ i.name }}</td>
+            <td>
+              <button title="Restore"
+                @click="undo(i.id)"
+                class="btn btn-primary btn-xs btn-sm mr-1"
+                type="button"
+              >
+                <i class="fa fa-undo"></i>
+              </button>
+              <button title="Delete Permanently??" @click="permanentlyDelete(i.id)" class="btn btn-danger btn-xs btn-sm ml-1" type="button">
+                <i class="fa fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4
+        v-if="deletedItems.length < 1"
+        class="text-center"
+        style="color: rgb(94, 94, 94);"
+      >
+        Empty
+      </h4>
     </div>
     <div class="col-md-4"></div>
   </div>
@@ -79,7 +121,8 @@ export default {
       items: [
         { id: 1, name: 'Umesh Thapa', isActive: true },
         { id: 2, name: 'Ramesh Adhikari', isActive: true }
-      ]
+      ],
+      deletedItems: []
     }
   },
   methods: {
@@ -95,13 +138,37 @@ export default {
       }, 200)
     },
     deleteLists (id) {
+      var data = this.items.find(a => a.id === id)
       const index = this.items.findIndex(_ => _.id === id)
-      this.items.splice(index, 1)
+      if (index > -1) {
+        data.id = this.deletedItems.length + 1
+        this.deletedItems.push(data)
+      }
+      setTimeout(() => {
+        this.items.splice(index, 1)
+      }, 300)
     },
 
     editList (id) {
       const data = this.items.find(_ => _.id === id)
       this.listName = data.name
+    },
+
+    undo (id) {
+      var data = this.deletedItems.find(a => a.id === id)
+      const index = this.deletedItems.findIndex(_ => _.id === id)
+      if (index > -1) {
+        data.id = this.items.length + 1
+        this.items.push(data)
+      }
+      setTimeout(() => {
+        this.deletedItems.splice(index, 1)
+      }, 300)
+    },
+
+    permanentlyDelete (id) {
+      const index = this.deletedItems.findIndex(_ => _.id === id)
+      this.deletedItems.splice(index, 1)
     }
   }
 }
